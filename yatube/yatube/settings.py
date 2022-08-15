7563ebb2603db275d49218d543ba9685758dc671
Bogdan Avdeenko <bogdan.avdeenko@gmail.com>
2022-08-15 23:48:14 +0300

    fixed about and readme, added .env

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,10 +26,10 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uasd0x=7lgmv&12+50sd@)ki&v)-fo^yxr=7xs6r9%ol)#ej34'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
