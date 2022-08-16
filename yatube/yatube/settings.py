fa0071249a7cc87e037f61460d8304bfc6da57d7
Bogdan Avdeenko <bogdan.avdeenko@gmail.com>
2022-08-16 12:00:31 +0300

    final

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -29,7 +29,7 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
